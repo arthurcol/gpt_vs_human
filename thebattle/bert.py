3c780a011aee8321d3f275d62e8c6839a0e9d43f
Arthur Collard <arthur.collard@hotmail.fr>
2023-02-26 16:12:17 +0100

    bert encoding

--- thebattle/bert.py
+++ thebattle/bert.py
@@ -1,3 +1,7 @@
+import tensorflow as tf
+from transformers import BertTokenizerFast, TFBertModel
+
+
 import dask
 import os
 
@@ -6,37 +10,50 @@ from data import get_ds, get_X_y, write_tfrecords
 from embeder import embed_corpus
 from dask.diagnostics import ProgressBar
 
-from gensim.downloader import load
-
 
 df = get_ds(os.environ["PATH_DATA"])
 X, y = get_X_y(df)
 
+chunk_size = 500
+
 if os.environ["MODE"] == "DEV":
     X, y = X[:100], y[:100]
     chunk_size = 10
-
-chunk_size = 500
 
 
 def chunkify(X, y, n):
     return [(X[i : i + n], y[i : i + n]) for i in range(0, len(X), n)]
 
 
-def wraper(X, y, filename, wv):
-    vect_pad = embed_corpus(X, wv)
+tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
+bert = TFBertModel.from_pretrained("bert-base-uncased")
+
+
+def bert_embeder(X, tokenizer, bert_model):
+    tokens = tokenizer(
+        list(X),
+        padding="max_length",
+        max_length=256,
+        truncation=True,
+        return_tensors="tf",
+    )
+    vect = bert_model.predict(dict(tokens))
+    return vect["pooler_output"]
+
+
+def wraper(X, y, filename):
+    vect_pad = bert_embeder(X.text, tokenizer=tokenizer, bert_model=bert)
     write_tfrecords(X, vect_pad, y, filename)
 
 
 lazy_results = []
 chunk_n = 1
 
-wv = load("glove-wiki-gigaword-100")
 
 for chunk in chunkify(X, y, chunk_size):
     print(chunk_n)
     chunk_x, chunk_y = chunk[0], chunk[1]
-    r = dask.delayed(wraper)(chunk_x, chunk_y, f"dataset_{chunk_n}", wv)
+    r = dask.delayed(wraper)(chunk_x, chunk_y, f"bertds_{chunk_n}")
     lazy_results.append(r)
     chunk_n += 1
 
